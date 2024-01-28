a1e9b041c21d7ef57a1aa1d1e4eb0e79c757cc26
Jonny Borges <35742643+jonataslaw@users.noreply.github.com>
2024-01-28 04:37:30 -0300

    try remove the plugin for the last time

--- Plugins/Source/ReplicatedVitalityPlugin/Private/Core/LandingAction.cpp
+++
@@ -1,8 +0,0 @@
-﻿//   Copyright Vinipi Studios 2024. All rights reserved.
-
-
-#include "Core/LandingAction.h"
-
-void ULandingAction::OnActionActivated_Implementation(AActor* Owner, const float HorizontalSpeed)
-{
-}
